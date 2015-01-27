ade40b273777b0ed9ad87529eef07960b5fa172a
Ian <iantskon@gmail.com>
2015-01-27 10:01:04 -0800

    Write passing code for post tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+	validates :place, presence: true
+	validates :time, presence: true
+	validates :body, presence: true
+	validates :user_id, presence: true
+	validates :neighborhood, presence: true
 end
