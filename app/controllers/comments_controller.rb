5edec02b6287e199ae14060d9856358cda1741b2
Ian <iantskon@gmail.com>
2015-01-28 18:27:59 -0800

    Add current broken code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,20 @@ class CommentsController < ApplicationController
 		@post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
-    respond_to do |format|
+    # respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.js   {}
-        format.json { render json: @comment, status: :created, location: @comment }
+      	render json:@comment
+        # format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        # format.js   {}
+        # format.json { render json: @comment, status: :created, location: @comment }
+        # binding.pry
       else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        #format.html { render action: "new" }
+        render json: @comment.errors
+        #format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
   end
+
 
   private
     def comment_params
