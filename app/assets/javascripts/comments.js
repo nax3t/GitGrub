40c80983b03ec612677ecfbf4788d9bcf34d6689
Ian <iantskon@gmail.com>
2015-01-29 00:11:09 -0800

    Update master branch

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,15 +1,15 @@
-$(function() {
-	$("#ajaxbutton").click(function(e) {
-		e.preventDefault();
-		var getComment = $("#comment_body").val();
-		var getPostId = $("#hidden_post").val();
-		$("#comment_body").val("")
-		$.post("/comments.json", {comment:{comment_body: getComment, post_id: getPostId}}).done(function(data) {
-			// console.log(data)
-			$(".comments").append("<p>" + data.comment_body + "</p>")
-			UnobtrusiveFlash.flashOptions['timeout'] = 2000; // milliseconds
-			UnobtrusiveFlash.showFlashMessage('You have successfully added a comment!', {type: 'notice'});
-		})
-	})
-});
+// $(function() {
+// 	$("#ajaxbutton").click(function(e) {
+// 		e.preventDefault();
+// 		var getComment = $("#comment_body").val();
+// 		var getPostId = $("#hidden_post").val();
+// 		$("#comment_body").val("")
+// 		$.post("/comments.json", {comment:{comment_body: getComment, post_id: getPostId}}).done(function(data) {
+// 			// console.log(data)
+// 			$(".comments").append("<p>" + data.comment_body + "</p>")
+// 			UnobtrusiveFlash.flashOptions['timeout'] = 2000; // milliseconds
+// 			UnobtrusiveFlash.showFlashMessage('You have successfully added a comment!', {type: 'notice'});
+// 		})
+// 	})
+// });
 
